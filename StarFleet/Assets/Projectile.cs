e271866fe800b84a26a99c811b6cf6b69b2889fc
jimmy0627 <yic060627@gmail.com>
2025-08-06 21:26:01 +0800

    8/6 update
    更新Projectile.cs:將子彈換成直線射出

--- StarFleet/Assets/Projectile.cs
+++ StarFleet/Assets/Projectile.cs
@@ -3,38 +3,41 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    private Transform Target;
-    private Vector2 launchpos;
+    public Transform Target;
     private int ProjectileSpeed;
     private float FireRange;
     private float current = 0;
     private float maxtime;
+    private Vector3 DIR;
 
     //設定參數
-    void Awake() 
+    void Awake()
     {
         Target = transform.parent.GetComponent<Attack>().attacking.transform;
         ProjectileSpeed = transform.parent.GetComponent<Attack>().ProjectileSpeed;
-        launchpos = transform.position;
         FireRange = transform.parent.GetComponent<Attack>().FireRange;
         maxtime = FireRange / ProjectileSpeed;
+
+        Vector3 TargetDIR = Target.position - transform.position;
+        DIR = Vector3.RotateTowards(transform.up, TargetDIR, 2, 0.0f).normalized;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, DIR);
     }
     void FixedUpdate()
     {
         //子彈朝目標飛行
         if (Target != null)
         {
-            transform.Translate((Target.position - transform.position) * Time.deltaTime * ProjectileSpeed);
-            if (Vector2.Distance(Target.position, transform.position) <= 1 || Vector2.Distance(launchpos, transform.position) > FireRange)
+            transform.Translate(Vector3.up * Time.deltaTime * ProjectileSpeed);
+            if (Vector3.Distance(transform.position, Target.transform.position) <= 0.5f)  //近炸引信
             {
                 Destroy(gameObject);
             }
         }
-        //超過理論最大飛行時長後自毀
         current += Time.deltaTime;
         if (current > +maxtime)
         {
-            Destroy(gameObject);
+            Destroy(gameObject);  //超過理論最大飛行時長後自毀
         }
+        Debug.DrawRay(transform.position, DIR, Color.red);
     }
 }
